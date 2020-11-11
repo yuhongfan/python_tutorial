80fe1f294e45e5fb643110f26ca5ca111356d933
Yuhong Fan <yfan@ucar.edu>
2020-11-11 15:08:28 -0700

    Compare wind chill factors

--- mysci.py
+++ mysci.py
@@ -1,8 +1,9 @@
 # Column names and column indices to read
-columns = {'date':0, 'time':1, 'tempout':2, 'windspeed':7}
+columns = {'date':0, 'time':1, 'tempout':2, 'windspeed':7,
+           'windchill':12}
 
 # Data types for each column (only if non-string)
-types = {'tempout': float, 'windspeed': float}
+types = {'tempout': float, 'windspeed': float, 'windchill': float}
 
 # Initialize my data variable
 data = {}
@@ -42,3 +43,6 @@ windchill = []
 for temp, windspeed in zip(data['tempout'], data['windspeed']):
     windchill.append(compute_windchill(temp, windspeed))
 
+# DEBUG
+for wc_data, wc_comp in zip(data['windchill'], windchill):
+   print(f'{wc_data:.5f}   {wc_comp:.5f}   {wc_data - wc_comp:.5f}')
