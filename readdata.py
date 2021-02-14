fc784157b9f4588a27be6c21a297bc48cbc46fff
Yuhong Fan <yfan@ucar.edu>
2021-02-13 20:34:56 -0700

    Adding new readdata module

---
+++ readdata.py
@@ -0,0 +1,34 @@
+def read_data(columns, types={}, filename="data/wxobs20170821.txt"):
+   """
+   Read data from CU Boulder Weather Station data file
+
+   Parameters:
+      columns: A dictionary of column names mapping to column indices
+      types: A dictionary of column names mapping to types to which
+         to convert each column of data
+      filename: The string path pointing to the CU Boulder Weather
+            Station data file
+   """
+
+   # Initialize my data variable
+   data = {}
+   for column in columns:
+      data[column] = []
+
+   # Read the data file
+   with open(filename, 'r') as datafile:
+
+      # read the first three lines (header)
+      for _ in range(3):
+         datafile.readline()
+
+      # read and parse the rest of the file
+      for line in datafile:
+         split_line = line.split()
+         for column in columns:
+            i = columns[column]
+            t = types.get(column, str)
+            value = t(split_line[i])
+            data[column].append(value)
+
+   return data
