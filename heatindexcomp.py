01fcdd77ff491485731f597f0aa859c0bfbcba95
Yuhong Fan <yfan@ucar.edu>
2021-02-13 20:44:01 -0700

    Refactor scripts to use new module

--- heatindexcomp.py
+++ heatindexcomp.py
@@ -1,3 +1,5 @@
+from readdata import read_data
+
 # Column names and column indices to read
 columns = {'date':0, 'time':1, 'tempout':2, 'humout':5,
            'heatindex':13}
@@ -5,27 +7,8 @@ columns = {'date':0, 'time':1, 'tempout':2, 'humout':5,
 # Data types for each column (only if non-string)
 types = {'tempout': float, 'humout': float, 'heatindex': float}
 
-# Initialize my data variable
-data = {}
-for column in columns:
-    data[column] = []
-
-# Read the data file
-filename = "data/wxobs20170821.txt"
-with open(filename, 'r') as datafile:
-
-    # read the first three lines (header)
-    for _ in range(3):
-        datafile.readline()
-
-    # read and parse the rest of the file
-    for line in datafile:
-        split_line = line.split()
-        for column in columns:
-            i = columns[column]
-            t = types.get(column, str)
-            value = t(split_line[i])
-            data[column].append(value)
+# Read data from file
+data = read_data(columns, types=types)
 
 # Compute the heat index
 def compute_heatindex(t, hum):
--- windchillcomp.py
+++ windchillcomp.py
@@ -1,3 +1,5 @@
+from readdata import read_data
+
 # Column names and column indices to read
 columns = {'date':0, 'time':1, 'tempout':2, 'windspeed':7,
            'windchill':12}
@@ -5,27 +7,8 @@ columns = {'date':0, 'time':1, 'tempout':2, 'windspeed':7,
 # Data types for each column (only if non-string)
 types = {'tempout': float, 'windspeed': float, 'windchill': float}
 
-# Initialize my data variable
-data = {}
-for column in columns:
-    data[column] = []
-
-# Read the data file
-filename = "data/wxobs20170821.txt"
-with open(filename, 'r') as datafile:
-
-    # read the first three lines (header)
-    for _ in range(3):
-        datafile.readline()
-
-    # read and parse the rest of the file
-    for line in datafile:
-        split_line = line.split()
-        for column in columns:
-            i = columns[column]
-            t = types.get(column, str)
-            value = t(split_line[i])
-            data[column].append(value)
+# Read data from file
+data = read_data(columns, types=types)
 
 # Compute the wind chill temperature
 def compute_windchill(t, v):
