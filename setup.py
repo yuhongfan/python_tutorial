725d910b851cda8b5fd85c5af7b3d790bd1f3bbd
Yuhong Fan <yfan@ucar.edu>
2021-02-18 18:31:46 -0700

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="mysci",
+    version="1.0.0",
+    description="A sample package",
+    author="yuhong",
+    author_email="yfan@ucar.edu",
+    packages=["mysci"],
+    install_requires=[])
